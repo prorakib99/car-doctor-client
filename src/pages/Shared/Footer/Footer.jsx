b2c53eddb24c9d86895bf9d2d639b7683d3ac484
Md Rakibul Islam <bdsky25@gmail.com>
2024-01-09 13:50:54 +0600

    Implement Footer wrap in the container

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -1,33 +1,36 @@
 import logo from '../../../assets/footer-logo.png';
 const Footer = () => {
     return (
-        <footer className="footer lg:grid-cols-[4fr_8fr] py-12 lg:py-24 gap-10 lg:gap-40 justify-between p-10 bg-neutral-900 text-base-content">
-            <div>
-                <img src={logo} alt="" />
-                <p className="text-gray-200 text-base font-normal font-['Inter'] leading-relaxed">Edwin Diaz is a software and web technologies engineer, a life coach trainer who is also a serial .</p>
+        <footer className="bg-neutral-900 ">
+            <div className='container mx-auto px-8'>
+                <div className='footer lg:grid-cols-[4fr_8fr] py-12 lg:py-32 gap-10 lg:gap-40 justify-between text-base-content'>
+                    <div>
+                        <img src={logo} alt="" className='mb-2' />
+                        <p className="text-gray-200 text-base font-normal font-['Inter'] leading-relaxed">Edwin Diag is a software and web technologies engineer, a life coach trainer who is also a serial .</p>
+                    </div>
+                    <div className='grid md:flex gap-10 w-full lg:justify-between'>
+                        <div className='flex flex-col gap-2'>
+                            <h4 className="text-white mb-5 text-xl font-semibold font-['Inter']">About</h4>
+                            <a className="text-zinc-100 text-base font-normal font-['Inter'] link-hover">Home</a>
+                            <a className="text-zinc-100 text-base font-normal font-['Inter'] link-hover">Service</a>
+                            <a className="text-zinc-100 text-base font-normal font-['Inter'] link-hover">Contact</a>
+                        </div>
+                        <div className='flex flex-col gap-2'>
+                            <h4 className="text-white mb-5 text-xl font-semibold font-['Inter']">Company</h4>
+                            <a className="text-zinc-100 text-base font-normal font-['Inter'] link-hover">Why Car Doctor</a>
+                            <a className="text-zinc-100 text-base font-normal font-['Inter'] link-hover">About</a>
+
+                        </div>
+                        <div className='flex flex-col gap-2'>
+                            <h4 className="text-white mb-5 text-xl font-semibold font-['Inter']">Support</h4>
+                            <a className="text-zinc-100 text-base font-normal font-['Inter'] link-hover">Support Center</a>
+                            <a className="text-zinc-100 text-base font-normal font-['Inter'] link-hover">Feedback</a>
+                            <a className="text-zinc-100 text-base font-normal font-['Inter'] link-hover">Accessability</a>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className='grid md:flex gap-10 w-full lg:justify-between'>
-                <div className='flex flex-col gap-2'>
-                    <h4 className="text-white mb-5 text-xl font-semibold font-['Inter']">Services</h4>
-                    <a className="text-zinc-100 text-base font-normal font-['Inter'] link-hover">Branding</a>
-                    <a className="text-zinc-100 text-base font-normal font-['Inter'] link-hover">Design</a>
-                    <a className="text-zinc-100 text-base font-normal font-['Inter'] link-hover">Marketing</a>
-                    <a className="text-zinc-100 text-base font-normal font-['Inter'] link-hover">Advertisement</a>
-                </div>
-                <div className='flex flex-col gap-2'>
-                    <h4 className="text-white mb-5 text-xl font-semibold font-['Inter']">Company</h4>
-                    <a className="text-zinc-100 text-base font-normal font-['Inter'] link-hover">About us</a>
-                    <a className="text-zinc-100 text-base font-normal font-['Inter'] link-hover">Contact</a>
-                    <a className="text-zinc-100 text-base font-normal font-['Inter'] link-hover">Jobs</a>
-                    <a className="text-zinc-100 text-base font-normal font-['Inter'] link-hover">Press kit</a>
-                </div>
-                <div className='flex flex-col gap-2'>
-                    <h4 className="text-white mb-5 text-xl font-semibold font-['Inter']">Legal</h4>
-                    <a className="text-zinc-100 text-base font-normal font-['Inter'] link-hover">Terms of use</a>
-                    <a className="text-zinc-100 text-base font-normal font-['Inter'] link-hover">Privacy policy</a>
-                    <a className="text-zinc-100 text-base font-normal font-['Inter'] link-hover">Cookie policy</a>
-                </div>
-            </div>
+
         </footer>
     );
 };
